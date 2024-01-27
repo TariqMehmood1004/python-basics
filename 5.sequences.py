4d28dbcefe6301eee778749cc9ed105e2a76b24c
TariqMehmood1004 <116074810+TariqMehmood1004@users.noreply.github.com>
2024-01-27 13:19:57 +0500

    sequences and strings

---
+++ 5.sequences.py
@@ -0,0 +1,17 @@
+
+# String Sequence
+name = "Tariq Mehmood (John Brrighte)"
+print(name)
+print(name[10])
+print(name[15])
+
+# List Sequence
+name_list = [
+    "Tariq Mehmood", 
+    "Ali Ahmad", 
+    "Hamza"
+]
+
+print(name_list)
+print(name_list[1])
+print(name_list[0])
---
+++ 6.strings.py
@@ -0,0 +1,15 @@
+# String 
+    # Concatenations 
+    # Interpolations
+    
+# Concatenations
+print(2, " and ", 3)
+print("Tariq" , "Mehmood")
+
+# Interpolations
+first = "Tariq"
+last = "Mehmood"
+print(f"{first}, {last}")
+
+print(f"{2 + 3} = 5")
+
