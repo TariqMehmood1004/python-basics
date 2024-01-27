01364e86b079b8ffe71ba25ee9375ab0380c4da9
TariqMehmood1004 <116074810+TariqMehmood1004@users.noreply.github.com>
2024-01-27 13:28:00 +0500

    conditions

---
+++ 7.conditions.py
@@ -0,0 +1,15 @@
+'''
+    Conditions:
+    1. if-condition
+    2. if-else condition
+    3. if-elif-else condition
+'''
+number = int(input("Enter your number: "))
+
+if (number > 0):
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
+
